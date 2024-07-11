66a0dad33943e1d349dd8dd957a902dbe943cba3
Abhishek shukla <abhishekshukla824@gmail.com>
2024-07-11 23:17:52 +0530

    commit-repository-layer-methods

--- store/car/car.go
+++ store/car/car.go
@@ -1,1 +1,38 @@
-package store
+package car
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/abhis3110/carZone/models"
+)
+
+type Store struct { // a types of constructor
+	db *sql.DB
+}
+
+
+func new(db *sql.DB) Store {
+	return Store{db:db}
+}
+
+
+func(s Store) GetCarById(ctx context.Context, id string) (models.Car, error) {
+
+}
+
+func(s Store) GetCarByBrand(ctx context.Context, brand string, isEngine bool) (models.Car, error) {
+
+}
+
+func(s Store) CreateCar(ctx context.Context, carReq *models.CarRequest) (models.Car, error) {
+
+}
+
+func(s Store) UpdateCar(ctx context.Context, id string, carReq *models.CarRequest) (models.Car, error) {
+
+}
+
+func(s Store) DeleteCar(ctx context.Context, id string) (models.Car, error) {
+
+}
--- store/engine/engine.go
+++ store/engine/engine.go
@@ -1,1 +1,33 @@
-package store 
+package engine
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/abhis3110/carZone/models"
+)
+
+type EngineStore struct {
+	db *sql.DB
+}
+
+func New(db *sql.DB) *EngineStore {
+	return &EngineStore{db: db}
+}
+
+func (e EngineStore) EngineById(ctx context.Context, id string) (models.Engine, error) {
+
+}
+
+func (e EngineStore) CreateEngine(ctx context.Context, engineReq *models.EngineRequest) (models.Engine, error) {
+	
+}
+
+func (e EngineStore) UpdateEngine(ctx context.Context, id string, engineReq *models.EngineRequest) (models.Engine, error) {
+	
+}
+
+
+func (e EngineStore) DeleteEngine(ctx context.Context, id string) (models.Engine, error) {
+	
+}
