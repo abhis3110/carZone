b9d4b27d118131232339a2d20708326f905b645a
Abhishek shukla <abhishekshukla824@gmail.com>
2024-07-12 21:12:13 +0530

    commit-service-layer

--- service/engine/engine.go
+++ service/engine/engine.go
@@ -1,1 +1,63 @@
-package service 
+package engine
+
+import (
+	"context"
+
+	"github.com/abhis3110/carZone/models"
+	"github.com/abhis3110/carZone/store"
+)
+
+type EngineService struct {
+	store store.EngineStoreInterface
+}
+
+
+func NewEngineService(store store.EngineStoreInterface) *EngineService {
+	return &EngineService {
+		store : store,
+	}
+}
+
+
+
+func (s *EngineService) GetEngineByID(ctx context.Context, id string) (*models.Engine, error) {
+	engine, err := s.store.EngineById(ctx, id)
+	if err != nil {
+		return nil, err
+	}
+	return &engine, nil
+}
+
+
+func (s *EngineService) CreateEngine(ctx context.Context, engineReq *models.EngineRequest) (*models.Engine, error) {
+	if err := models.ValidateEngineRequest(*engineReq); err != nil {
+		return nil, err
+	}
+	createdEngine, err := s.store.CreateEngine(ctx, engineReq)
+	if err != nil {
+		return nil, err
+	}
+	return &createdEngine, nil
+}
+
+func (s *EngineService) UpdateEngine(ctx context.Context, id string, engineReq *models.EngineRequest) (*models.Engine, error) {
+	if err := models.ValidateEngineRequest(*engineReq); err != nil {
+		return nil, err
+	}
+	updatedEngine, err := s.store.UpdateEngine(ctx, id, engineReq)
+	if err != nil {
+		return nil, err
+	}
+	return &updatedEngine, nil
+}
+
+
+
+func (s *EngineService) DeleteEngine(ctx context.Context, id string) (*models.Engine, error) {
+	deletedEngine, err := s.store.DeleteEngine(ctx, id)
+	if err != nil {
+		return nil, err
+	}
+	return &deletedEngine, nil
+
+}
--- store/interface.go
+++ store/interface.go
@@ -1,1 +1,27 @@
 package store
+
+import (
+	"context"
+
+	"github.com/abhis3110/carZone/models"
+)
+
+
+type CarStoreInterface interface {
+	GetCarById(ctx context.Context, id string) (models.Car, error)
+	GetCarByBrand(ctx context.Context, brand string, isEngine bool) ([]models.Car, error) 
+	CreateCar(ctx context.Context, carReq *models.CarRequest) (models.Car, error)
+	UpdateCar(ctx context.Context, id string, carReq *models.CarRequest) (models.Car, error)
+	DeleteCar(ctx context.Context, id string) (models.Car, error) 
+
+}
+
+
+type EngineStoreInterface interface {
+	EngineById(ctx context.Context, id string) (models.Engine, error)
+	CreateEngine(ctx context.Context, engineReq *models.EngineRequest) (models.Engine, error)
+	UpdateEngine(ctx context.Context, id string, engineReq *models.EngineRequest) (models.Engine, error)
+	DeleteEngine(ctx context.Context, id string) (models.Engine, error)
+
+}
+ 
